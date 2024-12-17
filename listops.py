71103bc2c7e760622d4557c966454c3477962a51
annewak <wakomoanne@gmail.com>
2024-12-17 15:08:45 +0000

    Add files via upload

---
+++ listops.py
@@ -0,0 +1,6 @@
+int_list = [80, 36, 2, 46, 12, 1, 94, 24, 75, 13]
+print(int_list)
+print("Sum =", sum(int_list))
+print("Largest =", max(int_list))
+print("Reversed =", int_list[::-1])
+print("Smallest =", min(int_list))
---
+++ loops.py
@@ -0,0 +1,7 @@
+#n = whole number
+n = int(input("Enter a non-negative integer: "))
+def factorial(x)
+if x < 0:
+    for n in range ():
+    factorial = factorial * n
+print(n,":", factorial)
