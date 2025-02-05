c3381c18153ab52ee5377d92cc8050734a4a1ed3
annewak <wakomoanne@gmail.com>
2025-02-05 09:06:40 +0000

    debugging

---
+++ buggy_code.py
@@ -0,0 +1,30 @@
+#Incorrect operation used for addition
+def add_numbers(a, b):
+    return a - b
+
+#Incorrect operation used for subtraction
+def subtract_numbers(a, b):
+    return a + b
+
+#Missing colon at the end of the definition function
+def multiply_numbers(a, b)
+    return a * b
+
+print("The sum of 5 and 3 is: " + str(add_numbers(5, 6)))
+#Correcting the function call and adding a quote around the string
+print("The subtraction of 5 from 3 is: subtract_numbers(5, 6))
+#Adding quotes around the string     
+print(The multiplication of 5 and 3 is  + str(multiply_number(5, 6))
+
+#Revised code
+
+def add_numbers(a, b):
+    return a + b
+def subtract_numbers(a, b):
+    return a - b
+def multiply_numbers(a, b):
+    return a * b
+
+print("The sum of 5 and 3 is: " + str(add_numbers(5, 6)))
+print("The subtraction of 5 from 3 is:" + str(subtract_numbers(5, 6
+print("The multiplication of 5 and 3 is" + str(multiply_number(5, 6))))
